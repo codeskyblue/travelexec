ed04ef4164ad98d78339501bc713f91719c5e2c3
shxsun <shxsun@gmail.com>
2014-02-24 19:34:52 +0800

    finish first version

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"text/template"
 	"time"
 )
 
-func renderTemplate(filename string, tmpl *template.Template, data interface{}) (err error) {
+func renderTemplate(filename string, tmplFile string, data interface{}) (err error) {
+	tmpl, err := template.ParseFiles("rep.tmpl")
+	if err != nil {
+		return
+	}
 	fd, err := os.Create(filename)
 	if err != nil {
 		return err
@@ -58,29 +65,48 @@ var (
 	path     = flag.String("path", "./", "root path")
 	replacer = flag.String("i", "{}", "filename replacer")
 	command  = flag.String("c", "./{}", "spec how to run command")
+	outfile  = flag.String("o", "test.html", "output file")
 )
 
-func work(files []string) (err error) {
-	tmpl, err := template.ParseFiles("rep.tmpl")
-	if err != nil {
-		return
-	}
-	startTime := time.Now()
-	data := map[string]interface{}{
-		"StartTime": startTime.Format("2006-01-02 15:04:05"),
-	}
-	for i, file := range files {
-		c := strings.Replace(*command, *replacer, file, -1)
-		fmt.Printf("%d\t%s\t%s\n", i, file, c)
-	}
-	endTime := time.Now()
-	data["TimeCost"] = endTime.Sub(startTime)
+type TaskConfig struct {
+	Replacer string
+	Command  string
+	Files    []string
+}
 
-	err = renderTemplate("test.html", tmpl, data)
-	if err != nil {
-		return
+type TaskResult struct {
+	StartTime string
+	TimeCost  time.Duration
+	Command   string
+	Output    string // console out
+	Source    string // source code
+	Error     error
+}
+
+func work(cfg *TaskConfig) (results []TaskResult, err error) {
+	results = []TaskResult{}
+	for i, file := range cfg.Files {
+		start := time.Now()
+		r := TaskResult{
+			StartTime: start.Format("15:04:05"),
+		}
+		c := strings.Replace(cfg.Command, cfg.Replacer, file, -1)
+		fmt.Printf("%d\t%s\t%s\n", i, file, c)
+		output := bytes.NewBuffer(nil)
+		cmd := exec.Command("/bin/bash", "-c", c)
+		cmd.Stdout = io.MultiWriter(os.Stdout, output)
+		cmd.Stderr = io.MultiWriter(os.Stderr, output)
+		err = cmd.Run()
+		if err != nil {
+			r.Error = err
+		}
+		r.Command = c
+		r.TimeCost = time.Now().Sub(start)
+		r.Output = string(output.Bytes())
+		r.Source = "unfinished(todo)"
+		results = append(results, r)
 	}
-	return nil
+	return
 }
 
 func init() {
@@ -94,7 +120,34 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = work(files)
+	taskcfg := &TaskConfig{}
+	taskcfg.Command = *command
+	taskcfg.Replacer = *replacer
+	taskcfg.Files = files
+	results, err := work(taskcfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errCnt := 0
+	for _, r := range results {
+		if r.Error != nil {
+			errCnt++
+		}
+		//println(r.Command)
+		//fmt.Println(string(r.Output))
+	}
+	startTime := time.Now()
+	data := map[string]interface{}{}
+	data["StartTime"] = startTime.Format("2006-01-02 15:04:05")
+	endTime := time.Now()
+	data["TimeCost"] = endTime.Sub(startTime)
+	data["Tasks"] = results
+	hostname, _ := os.Hostname()
+	data["Host"] = hostname
+	data["Total"] = len(files)
+	data["FailCount"] = errCnt
+
+	err = renderTemplate(*outfile, "rep.tmpl", data)
 	if err != nil {
 		log.Fatal(err)
 	}
