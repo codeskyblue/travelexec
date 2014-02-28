ba74938a0f600b5b96fb2e3977502d54a791b67d
shxsun <shxsun@gmail.com>
2014-02-28 19:44:24 +0800

    add notify

--- notify.go
+++ notify.go
@@ -3,14 +3,21 @@ package main
 import (
 	"bytes"
 	"io"
+	"log"
 	"mime/multipart"
 	"net/http"
 	"os"
 )
 
-func postForm(url string, params map[string]string, files map[string][]byte) (err error) {
+func postForm(uri string, params map[string]string, files map[string][]byte) (err error) {
 	body := &bytes.Buffer{}
 	w := multipart.NewWriter(body)
+	for field, value := range params {
+		err = w.WriteField(field, value)
+		if err != nil {
+			return
+		}
+	}
 	for name, data := range files {
 		part, er := w.CreateFormFile(name, "rep.html")
 		if er != nil {
@@ -23,7 +30,11 @@ func postForm(url string, params map[string]string, files map[string][]byte) (er
 			return
 		}
 	}
-	req, err := http.NewRequest("POST", "http://www.baidu.com", body)
+	if err = w.Close(); err != nil {
+		return
+	}
+	//log.Println("boundary:", w.Boundary(), len(body.Bytes()))
+	req, err := http.NewRequest("POST", uri, body)
 	if err != nil {
 		return
 	}
@@ -32,9 +43,11 @@ func postForm(url string, params map[string]string, files map[string][]byte) (er
 	}
 
 	req.Header.Set("Content-Type", w.FormDataContentType())
+	//req.ContentLength += 68
 	var client http.Client
 	res, err := client.Do(req)
 	if err != nil {
+		log.Println(err)
 		return
 	}
 	defer res.Body.Close()
@@ -43,5 +56,17 @@ func postForm(url string, params map[string]string, files map[string][]byte) (er
 }
 
 func sendNotify(msg string, users ...string) (err error) {
+	params := map[string]string{
+		"username": "sunshengxiang01",
+		"tel":      "185123",
+	}
+	err = postForm("http://localhost:8080", params, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return nil
+}
+
+func init() {
+	//	sendNotify("hi body", "sunshengxiang01")
 }
